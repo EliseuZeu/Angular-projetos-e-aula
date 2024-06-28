c15c9cd78de67712a62b6e2873f33904a5f4285d
Eliseu <eliseu.souza@mail.usf.edu.br>
2024-06-28 11:31:54 -0300

    aplicando função e estilos global

--- fundamentos-angular/src/app/segundo-componente/segundo-componente.component.ts
+++ fundamentos-angular/src/app/segundo-componente/segundo-componente.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class SegundoComponenteComponent {
   nome = "Joao";
   dataNascimento = "1995-01-03";
+  urlImagem = "/fundamentos-angular/assets/.img";
+
+  //Mostrando a data de nascimento função
+  mostrarDataNascimento(){
+    alert(`A data de nascimento do joao e: ${this.dataNascimento}`)
+  }
 }
