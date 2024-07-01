a364a346dd83f9fe79bd8a7a83e8d891f035c896
Eliseu <eliseu.souza@mail.usf.edu.br>
2024-07-01 12:00:13 -0300

    adicionando serviço produto

--- fundamentos-angular2/fundamentos-angular2/src/app/app.module.ts
+++ fundamentos-angular2/fundamentos-angular2/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExemplosAngularComponent } from './exemplos-angular/exemplos-angular.component';
 import { FormsModule } from '@angular/forms';
+import { ExemploServicos2Component } from './exemplo-servicos2/exemplo-servicos2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExemplosAngularComponent
+    ExemplosAngularComponent,
+    ExemploServicos2Component
   ],
   imports: [
     BrowserModule,
