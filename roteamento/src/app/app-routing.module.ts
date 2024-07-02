1a2ab86ad8608ba6e4ba53e0398786dd446cdde6
Eliseu <eliseu.souza@mail.usf.edu.br>
2024-07-02 13:36:03 -0300

    laizy-loading carregar arquivos se forem necessario

--- roteamento/src/app/app-routing.module.ts
+++ roteamento/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- roteamento/src/app/pagina-com-parametros/pagina-com-parametros.component.ts
+++ roteamento/src/app/pagina-com-parametros/pagina-com-parametros.component.ts
@@ -1,27 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { __param } from 'tslib';
 
 @Component({
   selector: 'app-pagina-com-parametros',
   templateUrl: './pagina-com-parametros.component.html',
-  styleUrl: './pagina-com-parametros.component.css'
+  styleUrls: ['./pagina-com-parametros.component.css']
 })
-export class PaginaComParametrosComponent {
+export class PaginaComParametrosComponent implements OnInit {
   id: number | null = null;
   idade: number | null = null;
-  nome:string | null = "";
+  nome: string | null = null;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(__param => {
-      this.id = Number(__param.get("id"));
+    this.route.paramMap.subscribe(paramMap => {
+      this.id = Number(paramMap.get('id'));
     });
 
-    this.route.queryParamMap.subscribe(__param => {
-      this.idade = Number(__param.get("idade"));
-      this.nome = __param.get("nome");
-    })
+    this.route.queryParamMap.subscribe(queryParamMap => {
+      this.idade = Number(queryParamMap.get('idade'));
+      this.nome = queryParamMap.get('nome');
+    });
   }
 }
