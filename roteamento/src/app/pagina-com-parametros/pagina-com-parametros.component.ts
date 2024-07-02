492dbda6fea676117d808f950af4310fcdffbd0e
Eliseu <eliseu.souza@mail.usf.edu.br>
2024-07-02 13:24:56 -0300

    aidicionando idade e nome no parametros.

--- roteamento/src/app/pagina-com-parametros/pagina-com-parametros.component.ts
+++ roteamento/src/app/pagina-com-parametros/pagina-com-parametros.component.ts
@@ -9,12 +9,19 @@ import { __param } from 'tslib';
 })
 export class PaginaComParametrosComponent {
   id: number | null = null;
+  idade: number | null = null;
+  nome:string | null = "";
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(__param => {
       this.id = Number(__param.get("id"));
+    });
+
+    this.route.queryParamMap.subscribe(__param => {
+      this.idade = Number(__param.get("idade"));
+      this.nome = __param.get("nome");
     })
   }
 }
