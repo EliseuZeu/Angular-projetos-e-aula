b3bdf8931adda3619088d9392ac95bb33864f317
Eliseu <eliseu.souza@mail.usf.edu.br>
2024-08-23 15:36:39 -0300

    estilizando os componentes dinamicamente

--- ArvoreFantasma/src/CardNews.js
+++ ArvoreFantasma/src/CardNews.js
@@ -25,14 +25,17 @@ class CardNews extends HTMLElement{
         autor.textContent = "By " + (this.getAttribute("autor") || "By Anonimos");
 
         const linkDev = document.createElement("a");
+        linkDev.href = this.getAttribute("link-url");
         linkDev.textContent = this.getAttribute("title");
         const paragrafo = document.createElement("p");
         paragrafo.textContent = this.getAttribute("paragrafo");
 
         //conteudo dentro da card right
         const cardRight = document.createElement("div"); // HTML - <div></div>
-        cardLeft.setAttribute("class", "card-right");
+        cardRight.setAttribute("class", "card-right");
         const img = document.createElement("img");
+        img.src = this.getAttribute("img");
+        img.alt = "Imagem da noticia";
 
 
 
@@ -48,7 +51,48 @@ class CardNews extends HTMLElement{
 
     //metodo de stylos do componentes
     styles() {
+        const style = document.createElement("style");
+        style.textContent = `
+            .card {
+          width: 80%;
+          box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+          -webkit-box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+          -moz-box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+        }
+          img {
+            width: 400px;
+            heigth: 200px;
 
+          }
+        
+        .card-left {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          padding-left: 10px;
+        }
+        
+        .card-left > span {
+          font-weight: 400;
+        }
+        
+        .card-left > a {
+          margin-top: 15px;
+          font-size: 25px;
+          color: black;
+          text-decoration: none;
+          font-weight: bold;
+        }
+        
+        .card-left > p {
+          color: rgb(70, 70, 70);
+        }
+        `
+
+        return style;
     }
 }
 
